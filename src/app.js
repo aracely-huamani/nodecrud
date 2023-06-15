1b723a9ffadbb9ae16c5f0e9158a5f8dc2729e7e
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2023-06-15 16:01:21 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.engine('.hbs', engine({
 }));
 app.set('view engine', 'hbs');
 
-app.use(myconnection(mysql, {
+app.use(mysql.createPool({
   MYSQLHOST: MYSQLHOST,
   MYSQLUSER: MYSQLUSER,
   MYSQLPASSWORD: MYSQLPASSWORD,
